9ac717db6421099921a1e63258655a545f9a8ea4
soal <sorrow.about.alice@gmail.com>
2018-07-21 16:08:53 +0300

    Added index and reorganized model

--- model.js
+++ model.js
@@ -17,12 +17,11 @@ async function dbConnect() {
 }
 
 async function createModel() {
-  const db = await dbConnect()
-  const DistrictShchema = mongoose.Schema({
+  const districtShchema = mongoose.Schema({
     number: Number,
     title: String,
     shape: {
-      type: 'Polygon',
+      type: { type: String },
       coordinates: [
         [[Number, Number]],
         [[Number, Number]]
@@ -30,16 +29,27 @@ async function createModel() {
     }
   })
 
-  DistrictShchema.methods.checkPoint = function(point) {
+  districtShchema.index({ 'shape': '2dsphere' })
+
+  districtShchema.methods.checkPoint = function(point) {
     return isInPolygon(point, this.shape)
   }
 
-  const District = mongoose.model('District', DistrictShchema);
-  return { db, District }
+  const District = mongoose.model('District', districtShchema)
+  console.log('Model created')
+  return District
+}
+
+async function init() {
+  const db = await dbConnect()
+  console.log('DB connected!')
+  const districtModel = await createModel()
+
+  return { db, districtModel }
 }
 // db.on('error', console.error.bind(console, 'connection error:'))
+
 // db.once('open', () => {
 //   console.log('DB connected!')
 // })
-
-mudule.export = createModel()
+// mudule.export = await init()
